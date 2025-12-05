ef6e8799da3ab943d9fa55d50e31b3bea65cd445
Eron Moquete <eronmoqueet4@gmail.com>
2025-12-06 05:59:38 +0800

    Added API Key for open weather

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ const app = {
             options: { responsive: true, maintainAspectRatio: false, plugins: { legend: { position: 'right' } } }
         });
 
-        const apiKey = 
+        const apiKey = '087c63d4f2bdc8a29e9262521bb2cd38'; 
         const city = 'Manolo Fortich'; 
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
